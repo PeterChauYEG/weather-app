0830a86d32c803461eae1fcf14208e25fcab2967
peterchauyeg <peter.chau.esq@gmail.com>
2017-02-04 12:04:28 -0700

    templated

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ let data = "This is some test data";
 
 // define geolocation options
 const geoOptions = {
-	enableHighAccuracy: true,
-	timeout: 5000, // in ms so 5 seconds
-	maximumAge: 0
+
 };
 
 init();
@@ -16,72 +14,54 @@ init();
 // finally
 function init() {
   // Get the location
-  getGeolocation(geoOptions);
+
 }
 
 // get the user's geolocation
 // takes options object
 function getGeolocation(options) {
-	// make sure geolocation is supported
-	if (!navigator.geolocation) {
-		return "Geolocation is not supported by your browser";
-	}
 
-	// get the geolocation
-	navigator.geolocation.getCurrentPosition(geoSuccess, GeoError, options);
 }
 
 // on success
 function geoSuccess(pos) {
-	// RenderData(`Latitude: ${pos.coords.latitude}, Longitude: ${pos.coords.longitude}`);
 
-	// get the weather
-	getWeather(pos.coords.latitude, pos.coords.longitude);
 }
 
 // on error
 function GeoError(err) {
-	RenderData({"error": "No Data Received"});
+
 }
 
 // get weather data via http GET
 // takes latitude and longitude
 // finally we render it onto the screen
 function getWeather(latitude, longitude) {
-	// create request
-	let request = new XMLHttpRequest();
 
-	// define endpoint
-	const endpoint = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=3e3fc59ab891338262ec5c0baf66faca&units=metric`;
-	// const endpoint = `http://api.openweathermap.org/data/2.5/weather?q=Edmonton,ca&APPID=3e3fc59ab891338262ec5c0baf66faca&units=metric`;
+}
 
-	request.open('GET', endpoint, true);
+function renderWeatherData(data) {
+	// Render the retreived data to the HTML DOM
 
-	request.onload = function() {
+	const weatherData = JSON.parse(data);
 
-		const response = request.responseText;
 
-		if (request.status >= 200 && request.status < 400) {
+	// Get your HTML elements from the DOM in order to insert data
+	// locationName
+	// currentTemp
+	// temp_max
+	// temp_min
+	// description
 
-			// success
-			renderWeatherData(response);
+	// Insert data to DOM elements
+	locationName.innerHTML = weatherData.name;
+	// ...
 
-		} else {
+	// Apply unique weather icons and background styles
+	// based on various weather conditions
+	getWeatherIconAndBG(weatherData.weather[0].id);
 
-			// server error
-			RenderData(`Error: ${response}`);
-		}
-		return
-	}
-
-	request.onerror = function() {
-		const response = request.responseText;
-
-		// connection error
-		RenderData(`Error: ${response}`);
-	}
-
-	request.send();
+	return;
 }
 
 function getWeatherIconAndBG(weatherId) {
@@ -92,21 +72,10 @@ function getWeatherIconAndBG(weatherId) {
 	// unique icons and background images for improved
 	// user experience.
 
-	const weatherIconElement = document.getElementById('weatherIcon');
-	const body = document.body;
-
-	let weatherIcon = '';
-	let bodyColor = '';
+	// ...
 
 	switch(true) {
-		case weatherId >= 200 && weatherId <= 232:
-			bodyColor = 'darkGrey';
-			weatherIcon = 'wi wi-thunderstorm';
-			break;
-		case weatherId >= 300 && weatherId <= 321:
-			bodyColor = 'gentleRain';
-			weatherIcon = 'wi wi-showers';
-			break;
+		// .....
 		case weatherId >= 500 && weatherId <= 531:
 			bodyColor = 'sadBlue';
 			weatherIcon = 'wi wi-rain';
@@ -137,46 +106,7 @@ function getWeatherIconAndBG(weatherId) {
 			break;
 	}
 
-	weatherIconElement.className = weatherIcon;
-	body.className = bodyColor;
+  // ...
 
 	return;
-}
-
-function renderWeatherData(data) {
-	// Render the retreived data to the HTML DOM
-
-	const weatherData = JSON.parse(data);
-
-
-	// Get your HTML elements from the DOM in order to insert data
-	const locationName = document.getElementById('locationName');
-	const currentTemp = document.getElementById('currentTemp');
-	const temp_max = document.getElementById('temp_max');
-	const temp_min = document.getElementById('temp_min');
-	const description = document.getElementById('description');
-
-	// Insert data to DOM elements
-	locationName.innerHTML = weatherData.name;
-	currentTemp.innerHTML = Math.round(weatherData.main.temp) || weatherData.error;
-	temp_max.innerHTML = weatherData.main.temp_max;
-	temp_min.innerHTML = weatherData.main.temp_min;
-	description.innerHTML = weatherData.weather[0].description;
-
-	// Apply unique weather icons and background styles
-	// based on various weather conditions
-	getWeatherIconAndBG(weatherData.weather[0].id);
-
-	return;
-}
-
-// renders data onto the screen (#data) for testing purposes only
-// string data Takes the data you want to render
-function RenderData(data) {
-	// grab the target element
-	let dataElement = document.getElementById("data");
-
-	// now modify the element's inner html
-	// append the data variable
-	dataElement.innerHTML = data;
 }
